8bc84a21476a7b2206099e38f1f557b94cf53ae5
Ernesto H. Almeda Jr <mtgons5.ea@gmail.com>
2023-04-16 08:19:49 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# finance_srik<br>
+Install required modules for Python<br>
+Register for an API in NYTimes<br>
+Create file .env<br>
+NYT_API_KEY="YOUR API KEY"<br>
+pip install -r requirements.txt<br>
--- finance.py
+++ finance.py
@@ -15,23 +15,16 @@ params = {
 
 response = requests.get(url, params=params)
 
-# if response.status_code == 200:
-#     data = response.json()
-#     with open('nyt_articles.json', 'w') as f:
-#         json.dump(data, f)
-# else:
-#     print("Error retrieving data from API")
-
 if response.status_code == 200:
     data = response.json()
     articles = data["response"]["docs"]
     parsed_data = []
     for article in articles:
         parsed_article = {
-            "abstract": article.get("abstract", ""),
-            "web_url": article.get("web_url", ""),
-            "snippet": article.get("snippet", ""),
-            "lead_paragraph": article.get("lead_paragraph", ""),
+            "abstract": article['abstract'].replace('\u2019', "'"),
+            "web_url": article['web_url'].replace('\u2019', "'"),
+            "snippet": article['snippet'].replace('\u2019', "'"),
+            "lead_paragraph": article['lead_paragraph'].replace('\u2019', "'"),
             "source": article.get("source", ""),
             "pub_date": article.get("pub_date", ""),
             "document_type": article.get("document_type", ""),
@@ -39,7 +32,7 @@ if response.status_code == 200:
             "section_name": article.get("section_name", "")
         }
         parsed_data.append(parsed_article)
-    with open(os.path.join(os.getcwd(), "nyt_articles.json"), "w") as f:
+    with open(os.path.join(os.getcwd(), "nyt_articles.json"), "w") as f: #nyt_articles.json filename to save parsed data
         json.dump(parsed_data, f, indent=4)
 else:
     print("Error: Could not retrieve articles.")
